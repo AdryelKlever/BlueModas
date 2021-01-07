f43b9f980d96e21251dadee48942838a1a32d9bc
adryelklever <adryelklever3m@hotmail.com>
2021-01-07 12:20:40 -0300

    Configuração do Domain

---
+++ BlueModas/Domain/Services/ServiceProduct.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Interfaces.IntarfaceProduct;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class ServiceProduct
+    {
+
+        private readonly IProduct _IProduct;
+
+        public ServiceProduct(IProduct IProduct)
+        {
+            _IProduct = IProduct;
+        }
+
+        public async Task AddProduct(Produto produto)
+        {
+            var validaNome = produto.ValidarPropriedadeString(produto.NomeProduto, "Nome");
+
+            var validaValor = produto.ValidarPropriedadeDecimal(produto.Valor, "Valor");
+
+            if (validaNome && validaValor)
+            {
+                produto.Estado = true;
+                await _IProduct.Add(produto);
+            }
+        }
+
+        public async Task UpdateProduct(Produto produto)
+        {
+            var validaNome = produto.ValidarPropriedadeString(produto.NomeProduto, "Nome");
+
+            var validaValor = produto.ValidarPropriedadeDecimal(produto.Valor, "Valor");
+
+            if (validaNome && validaValor)
+            {
+                await _IProduct.Update(produto);
+            }
+        }
+    }
+}
